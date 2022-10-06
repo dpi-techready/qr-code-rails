18935ee6b7bb44c4be10e48b628c26c09984893a
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-10-06 19:08:17 +0000

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,40 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "my_templates/homepage.html.erb" })
   end
+
+  def text_qr 
+    # parameters: {"content"=>"something"}
+    @user_text = params.fetch("content")
+
+    @qrcode = RQRCode::QRCode.new(@user_text)
+
+    render({ :template => "my_templates/text.html.erb"})
+  end  
+
+  def url_qr
+
+    @user_text = params.fetch("content")
+
+    @qrcode = RQRCode::QRCode.new(@user_text)
+
+    render({ :template => "my_templates/url.html.erb"})
+  end
+  
+  def wifi_qr
+
+    @user_text = params.fetch("content")
+
+    @qrcode = RQRCode::QRCode.new("WIFI:T:WPA;S:#{@user_text};P:#{@user_text};;")
+
+    render({ :template => "my_templates/wifi.html.erb"})
+  end  
+
+  def sms_qr
+
+    @user_text = params.fetch("content")
+
+    @qrcode = RQRCode::QRCode.new("SMSTO: #{@user_text}:Hi #{@user_text}")
+
+    render({ :template => "my_templates/sms.html.erb"})
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ Rails.application.routes.draw do
   # Write your routes here. URLs to support:
   
   # /text
+  get("/text", {:controller => "application", :action => "text_qr"})
+
   # /url
+  get("/url", {:controller => "application", :action => "url_qr"})
+
   # /wifi
+  get("/wifi", {:controller => "application", :action => "wifi_qr"})
+
   # /sms
+  get("/sms", {:controller => "application", :action => "sms_qr"})
   
+  # homepage
   get("/", {:controller => "application", :action => "homepage"})
 
   # Solutions below.
